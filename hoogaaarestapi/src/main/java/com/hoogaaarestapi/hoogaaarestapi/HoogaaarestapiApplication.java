929e174fc5d1bf1d369256caa5b8b14b8e9ce25b
manish gautam <manish.gautam@hoogaaa.com>
2023-08-05 22:09:31 +0530

    first commit

---
+++ hoogaaarestapi/src/main/java/com/hoogaaarestapi/hoogaaarestapi/HoogaaarestapiApplication.java
@@ -0,0 +1,14 @@
+package com.hoogaaarestapi.hoogaaarestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoogaaarestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoogaaarestapiApplication.class, args);
+		System.out.println("Project Running...........");
+	}
+
+}
---
+++ hoogaaarestapi/src/test/java/com/hoogaaarestapi/hoogaaarestapi/HoogaaarestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoogaaarestapi.hoogaaarestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoogaaarestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
